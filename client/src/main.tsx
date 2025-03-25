f0b323e1c9c152c2a8e972ff08b1c01e87a4c4a3
bowman-crego <bowmanccrego@gmail.com>
2025-03-25 13:17:07 -0700

    initial setup commit

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,23 +2,23 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <App />,
-//     errorElement: <NotFound />,
-//     children: [
-//       {
-//         index: true,
-//         element: <Home />
-//       }, {
-//         path: '/matchup',
-//         element: <Matchup />
-//       }, {
-//         path: '/matchup/:id',
-//         element: <Vote />
-//       },
-//     ],
-//   },
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, {
+        path: '/matchup',
+        element: <Matchup />
+      }, {
+        path: '/matchup/:id',
+        element: <Vote />
+      },
+    ],
+  },
 ]);
 
 const rootElement = document.getElementById('root');
