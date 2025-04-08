2372fa48e8519643a5da67453737451eba8452ae
bowman-crego <bowmanccrego@gmail.com>
2025-04-07 19:53:28 -0700

    add habit modal is connected to the database

--- client/src/components/habitModal.tsx
+++ client/src/components/habitModal.tsx
@@ -1,40 +1,57 @@
+import { useMutation } from '@apollo/client';
 import React, { useState } from 'react';
-
+import { ADD_HABIT } from '../utils/mutations'; // Import the ADD_HABIT mutation
 interface HabitModalProps {
   isOpen: boolean;
   onClose: () => void;
   onAdd: (habit: string, frequency: number, unit: string) => void;
   habits: string[];
-  userName: string;
+  username: string;
 }
 
-const HabitModal: React.FC<HabitModalProps> = ({ isOpen, onClose, onAdd, userName }) => {
+const HabitModal: React.FC<HabitModalProps> = ({ isOpen, onClose, onAdd, username }) => {
   const [newHabit, setNewHabit] = useState('');
   const [selectedHabit, setSelectedHabit] = useState('');
   const [frequency, setFrequency] = useState<number | ''>('');
   const [unit, setUnit] = useState('');
-
+  const [addHabit] = useMutation(ADD_HABIT);
   const presetHabits = ['Exercise', 'Sleep', 'Reading', 'Gaming'];
 
   if (!isOpen) return null;
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (selectedHabit || newHabit.trim()) {
-      const habitToAdd = selectedHabit || newHabit;
+//TODO: Need to connect addHabit and import ADD_HABIT mutation to connect this data to the database
+const handleSubmit = async (e: React.FormEvent) => {
+  e.preventDefault();
+  const habitToAdd = selectedHabit || newHabit;
+    if (habitToAdd.trim() && frequency && unit.trim()) {
+      try {
+        await addHabit({
+          variables: {
+            input: {
+              habitText: habitToAdd,
+              habitUsername: username,
+              targetGoal: frequency,
+              targetGoalUnit: unit,
+              habitDate: new Date().toISOString(),
+            },
+          }
+        });
       onAdd(habitToAdd, Number(frequency), unit);
       setSelectedHabit('');
       setNewHabit('');
       setFrequency('');
       setUnit('');
+
+    } catch (error) {
+      console.error('Error adding habit:', error);
     }
-  };
+  }
+};
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
       <div className="bg-black rounded-lg p-6 w-96 text-white">
         {/* Greeting */}
-        <h2 className="text-xl font-bold mb-2">Hello, {userName}!</h2>
+        <h2 className="text-xl font-bold mb-2">Hello, {username}!</h2>
         <p className="mb-4">Please select your habit that you'd like to begin tracking.</p>
 
         <form onSubmit={handleSubmit} className="mb-4">
